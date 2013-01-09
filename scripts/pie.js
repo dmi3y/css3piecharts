acc76e7e710e8e0209684e9ec5153878bd14d869
Dmitry Lapshukov <lapshukov@gmail.com>
2013-01-09 03:48:35 -0500

    embed js to separate file, .gitignore

---
+++ scripts/pie.js
@@ -0,0 +1,23 @@
+// prototype on jsbin http://jsbin.com/ijolib/15/edit
+! function() {
+    var lessPieText = $('#lessPie').text();
+    $(function() {
+        var pieOneDataFill = $('#pieOne').attr('data-fill');
+        while (parseInt(pieOneDataFill) > 180) {
+            pieOneDataFill = (parseInt(pieOneDataFill) - 180) + 'deg';
+        }
+        while (parseInt(pieOneDataFill) < 0) {
+            pieOneDataFill = (parseInt(pieOneDataFill) + 180) + 'deg';
+        }
+        $('#lessPie').text(lessPieText.replace('#dataDegree#', pieOneDataFill));
+        less.refreshStyles();
+
+        // update % value
+        // 180deg         = 100%
+        // pieOneDataFill = x%
+
+        var percentValue = (parseInt(pieOneDataFill) * 100) / 180;
+        $('#pieOneLegend').find('span').text(Math.floor(percentValue) + '%').end().show();
+
+    });
+}()
